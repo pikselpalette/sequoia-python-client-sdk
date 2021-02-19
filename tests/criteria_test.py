afc1437977e53564d88ed4042de28b8e069f1a4f
Jose Ortiz <jose.ortiz@piksel.com>
2021-02-19 12:27:15 +0100

    METAFLOW-123: Unit test when the token is invalid and a new one is retrieved

--- tests/criteria_test.py
+++ tests/criteria_test.py
@@ -14,10 +14,25 @@ class TestCriteria(unittest.TestCase):
         assert len(my_criteria._get_inclusion_entries()[0]._get_fields()) == 1
         assert my_criteria._get_inclusion_entries()[0].resource_name == "assets"
 
+        my_criteria = criteria.Criteria().add_inclusion(criteria.Inclusion.resource("assets").fields("ref", "ref"))
+
+        assert len(my_criteria.inclusion_entries) == 1
+        assert len(my_criteria._get_inclusion_entries()[0]._get_fields()) == 1
+        assert my_criteria._get_inclusion_entries()[0].resource_name == "assets"
+
     def test_get_criteria_params_given_more_than_one_inclusion_then_inclusion_param_should_be_created_properly(self):
         my_criteria = criteria.Criteria()\
             .add(inclusion=criteria.Inclusion.resource("assets"))\
             .add(inclusion=criteria.Inclusion.resource("contents"))
+
+        params = my_criteria.get_criteria_params()
+
+        assert len(params) == 1
+        assert params == {"include": "assets,contents"}
+
+        my_criteria = criteria.Criteria()\
+            .add_inclusion(criteria.Inclusion.resource("assets"))\
+            .add_inclusion(criteria.Inclusion.resource("contents"))
 
         params = my_criteria.get_criteria_params()
 
@@ -34,9 +49,26 @@ class TestCriteria(unittest.TestCase):
         assert params["include"] == "assets"
         assert params["assets.fields"] == "name,ref"
 
+        my_criteria = criteria.Criteria()\
+            .add_inclusion(criteria.Inclusion.resource("assets").fields("ref", "name"))
+
+        params = my_criteria.get_criteria_params()
+
+        assert len(params) == 2
+        assert params["include"] == "assets"
+        assert params["assets.fields"] == "name,ref"
+
     def test_get_criteria_params_given_simple_criterion_then_param_should_be_created_properly(self):
         my_criteria = criteria.Criteria()\
             .add(criterion=criteria.StringExpressionFactory.field("contentRef").equal_to("theContentRef"))
+
+        params = my_criteria.get_criteria_params()
+
+        assert len(params) == 1
+        assert params["withContentRef"] == "theContentRef"
+
+        my_criteria = criteria.Criteria()\
+            .add_criterion(criteria.StringExpressionFactory.field("contentRef").equal_to("theContentRef"))
 
         params = my_criteria.get_criteria_params()
 
